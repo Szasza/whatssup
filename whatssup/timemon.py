3f49cf028f74012369621c1d6caf890eb9164234
Szasza Palmer <szasza@szasza.info>
2024-07-22 15:51:35 +1000

    finalise timemon, enrich PyPI

--- whatssup/timemon.py
+++ whatssup/timemon.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
+import getopt
 import os
 import sys
+import xmlrpc.client as xmlrpcclient
 
 from supervisor import childutils
 
 
 class Timeout:
-    def __init__(self):
-        self.args = sys.argv[1:]
+    def __init__(self, arguments):
+        self.args = arguments
         self.stdin = sys.stdin
         self.stdout = sys.stdout
         self.stderr = sys.stderr
@@ -26,19 +28,82 @@ class Timeout:
             process_infos = self.rpc.supervisor.getAllProcessInfo()
 
             for process_info in process_infos:
-                pid = process_info['pid']
                 name = process_info['name']
-                group = process_info['group']
                 uptime = process_info['now'] - process_info['start']
-                self.stderr.write('PID: %s, name: %s, group %s, uptime %s' % (pid, name, group, uptime))
+
+                if name in self.args['programs'] and uptime > self.args['programs'][name]:
+                    self.restart(name)
 
             self.stderr.flush()
 
             childutils.listener.ok(self.stdout)
 
+    def stop(self, name):
+        try:
+            self.rpc.supervisor.stopProcess(name)
+        except xmlrpcclient.Fault as e:
+            msg = ('Failed to stop process %s, exiting: %s' %
+                   (name, e))
+            self.stderr.write(str(msg))
+            raise
+
+    def start(self, name):
+        try:
+            self.rpc.supervisor.startProcess(name)
+        except xmlrpcclient.Fault as e:
+            msg = ('Failed to start process %s, exiting: %s' %
+                   (name, e))
+            self.stderr.write(str(msg))
+            raise
+
+    def restart(self, name):
+        self.stderr.write('Restarting %s\n' % name)
+
+        self.stop(name)
+        self.start(name)
+
+        self.stderr.write('Restarted %s\n' % name)
+
+
+def parse_program_arg(option, value):
+    try:
+        name, timeout = value.split('=')
+        timeout = int(timeout)
+    except ValueError:
+        print('Invalid value %r for %r' % (value, option))
+        name = None
+        timeout = None
+    return name, timeout
+
+
+def parse_args(arguments):
+    short_args = 'p:'
+    long_args = [
+        'program=',
+    ]
+    parsed_args = {}
+
+    if not arguments:
+        return parsed_args
+    try:
+        opts, args = getopt.getopt(arguments, short_args, long_args)
+    except:
+        return parsed_args
+
+    parsed_args['programs'] = {}
+
+    for option, value in opts:
+
+        if option in ('-p', '--program'):
+            name, timeout = parse_program_arg(option, value)
+            parsed_args['programs'][name] = timeout
+
+    return parsed_args
+
 
 def main():
-    timeout = Timeout()
+    parsed_args = parse_args(sys.argv[1:])
+    timeout = Timeout(parsed_args)
     timeout.rpc = childutils.getRPCInterface(os.environ)
     timeout.run()
 
